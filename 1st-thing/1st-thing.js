c99622d360b557b13da1dd7110e35e28f4cdbd66
esaminu <esaminu1@icloud.com>
2015-10-11 07:36:31 +0800

    Complete plan tomorrow todo list

--- 1st-thing/1st-thing.js
+++ 1st-thing/1st-thing.js
@@ -1,16 +1,43 @@
+todos = new Mongo.Collection('todos');
 if (Meteor.isClient) {
   // counter starts at 0
+  
   Session.setDefault('counter', 0);
-
-  Template.body.helpers({
-    
+  Template.planTomorrow.helpers({
+    todos: function() {
+      return todos.find();
+    }
+  });
+  Template.planTomorrow.events({
+    'click #addtodo': function() {
+      var todo = document.getElementById("todo").value;
+      todos.insert({todo: todo});
+    },
+    'click #remtodo': function() {
+      todos.remove(this._id);
+    }
+  });
+  Template.oneTimeSetup.helpers({
+    morningTime: function() {
+      return document.getElementById("dialog").hour();
+    },
+    nightTime: function() {
+      return document.getElementById("dialog1").time();
+    }
   });
 
-  Template.body.events({
+  Template.oneTimeSetup.events({
     'click .time': function() {
       var dialog = document.getElementById("dialog");
       dialog.open();
-    }
+    },
+    'click .time1': function() {
+      var dialog = document.getElementById("dialog1");
+      dialog.open();
+    },
+    'click .name': function() {
+      document.getElementById("nametext").setFocused(true);
+    },
   });
 
   }

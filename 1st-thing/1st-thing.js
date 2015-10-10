eef3da7de9490058ef4ba8ac499c7cfd989aede8
esaminu <esaminu1@icloud.com>
2015-10-10 22:15:16 +0800

    Complete first time setup screen

--- 1st-thing/1st-thing.js
+++ 1st-thing/1st-thing.js
@@ -2,19 +2,18 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    
+  });
+
+  Template.body.events({
+    'click .time': function() {
+      var dialog = document.getElementById("dialog");
+      dialog.open();
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+  }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {

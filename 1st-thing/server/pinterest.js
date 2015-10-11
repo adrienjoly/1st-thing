8abdf63dc76b0c549f46d9659b561d6bc7b6a079
Adrien Joly <adrien@whyd.com>
2015-10-11 15:24:36 +0700

    more concise pinterest code

--- 1st-thing/server/pinterest.js
+++ 1st-thing/server/pinterest.js
@@ -1,19 +1,16 @@
 var _ = lodash;
 
-function getPosterUrls() {
+var posters = (function getPosterUrls() {
   var result = HTTP.get('http://pinterest.com/explore/motivational-posters/');
   var $ = cheerio.load(result.content);
   return $('.pinImg').map(function(i){
-    console.log(i, $(this).attr('src'));
     return $(this).attr('src')
   });
-}
-
-var posters = getPosterUrls();
+})();
 
 Meteor.methods({
   posterUrl: function () {
-    this.unblock();
+    //this.unblock();
     return _.sample(posters);
   }
 });

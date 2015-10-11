edb678623867b06dcc0d7cfdc2fbcad853a68b4e
Adrien Joly <adrien@whyd.com>
2015-10-11 16:29:45 +0700

    setup: persisted times in db

--- 1st-thing/1st-thing.js
+++ 1st-thing/1st-thing.js
@@ -1,11 +1,23 @@
 todos = new Mongo.Collection('todos');
+settings = new Mongo.Collection('settings');
 
 if (Meteor.isClient) {  
   Session.setDefault('counter', 0);
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  if (!settings.findOne({ key: "morningTime" })) {
+    settings.insert({ key: "morningTime", value: "08:00" });
+  }
+  if (!settings.findOne({ key: "nightTime" })) {
+    settings.insert({ key: "nightTime", value: "20:00" });
+  }
+  Meteor.methods({
+    "setMorningTime": function(time) {
+      settings.upsert({key: "morningTime"}, {$set: {value: time}});
+    },
+    "setNightTime": function(time) {
+      settings.upsert({key: "nightTime"}, {$set: {value: time}});
+    },
   });
 }
--- 1st-thing/client/setup.js
+++ 1st-thing/client/setup.js
@@ -1,13 +1,16 @@
 Template.oneTimeSetup.helpers({
   morningTime: function() {
-    return document.getElementById("dialog").hour();
+    return (settings.findOne({key: "morningTime"}) || {}).value;
+    //return document.getElementById("dialog").hour();
   },
   nightTime: function() {
-    return document.getElementById("dialog1").time();
+    return (settings.findOne({key: "nightTime"}) || {}).value;
+    //return document.getElementById("dialog1").time();
   }
 });
 
 Template.oneTimeSetup.events({
+  /*
   'click .time': function() {
     var dialog = document.getElementById("dialog");
     dialog.open();
@@ -15,6 +18,15 @@ Template.oneTimeSetup.events({
   'click .time1': function() {
     var dialog = document.getElementById("dialog1");
     dialog.open();
+  },
+  */
+  'input #morning-time-selector': function(evt) {
+    console.log(evt.target.value);
+    Meteor.call("setMorningTime", evt.target.value);
+  },
+  'input #night-time-selector': function(evt) {
+    console.log(evt.target.value);
+    Meteor.call("setNightTime", evt.target.value);
   },
   'click .name': function() {
     document.getElementById("nametext").setFocused(true);

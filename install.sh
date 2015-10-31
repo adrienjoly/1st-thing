8cfabe1c11f26f8f9e2972e07434c5cead614b0b
Adrien Joly <adrien@whyd.com>
2015-10-31 20:26:51 +0700

    added landing page with mailchimp newsletter subscription box and google analytics tracking

--- install.sh
+++
@@ -1,6 +0,0 @@
-# from : https://help.github.com/articles/using-jekyll-with-pages/
-ruby --version           # must be at least 2.0
-gem install bundler      # installs bundle. may need to be run as root (sudo)
-bundle install           # runs Gemfile => installs github-pages
-# gem update github-pages
-# gem install jekyll
--- run.sh
+++
@@ -1,2 +0,0 @@
-# from : https://help.github.com/articles/using-jekyll-with-pages/
-bundle exec jekyll serve # serves the site locally

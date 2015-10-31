ab76844907b9f15141378ec1ef54457ff7ad39a6
Adrien Joly <adrien@whyd.com>
2015-10-31 18:13:37 +0800

    removed source code

--- app/client/iphone.js
+++
@@ -1,6 +0,0 @@
-Template.iphone.rendered = function(){
-  var ua = navigator.userAgent;
-  if (ua && ua.indexOf("Mobile") != -1 && ua.indexOf("iPad") == -1)
-    if (confirm("Go to mobile version?"))
-      top.location.href = "/";
-};
